23ae5e99ca11c77bde1952e7c4ec462ed3add233
padmini v <padmini.v@perfios.com>
2021-11-02 21:58:13 +0530

    ADDED: Employee model class

---
+++ src/main/java/com/bridgelabz/model/Employee.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.model;
+
+public class Employee 
+{
+	private String firstName;
+	private String lastName;
+
+	public String getFirstName() 
+	{
+		return this.firstName;
+	}
+
+	public String getLastName() 
+	{
+		return this.lastName;
+	}
+
+}
