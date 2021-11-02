d882f91639eac2b9f20c88c7cf8b95c20a7d1b67
padmini v <padmini.v@perfios.com>
2021-11-03 00:53:14 +0530

    ADDED: implemented update method

--- src/main/java/com/bridgelabz/service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/service/EmployeePayrollService.java
@@ -1,5 +1,6 @@
 package com.bridgelabz.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -7,38 +8,49 @@ import org.springframework.stereotype.Service;
 import com.bridgelabz.dto.EmployeePayrollDTO;
 import com.bridgelabz.model.EmployeePayrollData;
 
+
 @Service
 public class EmployeePayrollService implements IEmployeePayrollService 
 {
 
+	private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+
 	@Override
 	public List<EmployeePayrollData> getEmployeePayrollData() 
 	{
-		return null;
+		return employeePayrollList;
 	}
 
 	@Override
 	public EmployeePayrollData getEmployeePayrollDataById(int employeeId) 
 	{
-		return null;
+		return employeePayrollList.get(employeeId - 1);
 	}
 
 	@Override
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) 
 	{
-		return null;
+		EmployeePayrollData employeePayrollData = new EmployeePayrollData(
+				employeePayrollList.size() + 1,employeePayrollDTO);
+		employeePayrollList.add(employeePayrollData);
+		return employeePayrollData;
 	}
 
 	@Override
-	public EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) 
+	public EmployeePayrollData updateEmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) 
 	{
-		return null;
+		EmployeePayrollData employeePayrollData = this.getEmployeePayrollDataById(employeeId);
+		employeePayrollData.setName(employeePayrollDTO.name);
+		employeePayrollData.setSalary(employeePayrollDTO.salary);
+		employeePayrollList.set(employeeId - 1, employeePayrollData);
+		return employeePayrollData;
 	}
 
 	@Override
 	public void deleteEmployeePayrollData(int employeeId) 
 	{
-		
+		employeePayrollList.remove(employeeId - 1);
+
 	}
 
 }
