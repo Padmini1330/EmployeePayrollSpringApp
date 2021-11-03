d73208a0a2a1e98234077b74cd51e386af0230b9
padmini v <padmini.v@perfios.com>
2021-11-04 00:47:26 +0530

    MODIFIED: removed getters and setters, added @Data annotation

--- src/main/java/com/bridgelabz/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/model/EmployeePayrollData.java
@@ -1,50 +1,22 @@
 package com.bridgelabz.model;
 
 import com.bridgelabz.dto.EmployeePayrollDTO;
+import lombok.Data;
 
-public class EmployeePayrollData 
+public @Data class EmployeePayrollData 
 {
 	private int employeeId;
 	private String name;
 	private long salary;
 
-	public EmployeePayrollData() {}
-
-	public EmployeePayrollData(int employeeId,EmployeePayrollDTO employeePayrollDTO) 
+	public EmployeePayrollData() 
 	{
-	this.employeeId=employeeId;
-	this.name=employeePayrollDTO.name;
-	this.salary=employeePayrollDTO.salary;
 	}
 
-	public int getEmployeeId() 
-	{
-		return employeeId;
-	}
-
-	public void setEmployeeId(int employeeId) 
+	public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) 
 	{
 		this.employeeId = employeeId;
+		this.name = employeePayrollDTO.name;
+		this.salary = employeePayrollDTO.salary;
 	}
-
-	public String getName() 
-	{
-		return name;
-	}
-
-	public void setName(String name) 
-	{
-		this.name = name;
-	}
-
-	public long getSalary() 
-	{
-		return salary;
-	}
-
-	public void setSalary(long salary) 
-	{
-		this.salary = salary;
-	}
-
 }
