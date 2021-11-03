e77dd1ea894b3cd12a7b55f34b2fff3f85439293
padmini v <padmini.v@perfios.com>
2021-11-04 01:58:19 +0530

    MODIFIED: Service methods throw custom exceptions if incase employee id isnt found

--- src/main/java/com/bridgelabz/service/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/service/EmployeePayrollService.java
@@ -2,12 +2,10 @@ package com.bridgelabz.service;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Service;
-
 import com.bridgelabz.dto.EmployeePayrollDTO;
+import com.bridgelabz.exceptions.EmployeePayrollException;
 import com.bridgelabz.model.EmployeePayrollData;
-
 
 @Service
 public class EmployeePayrollService implements IEmployeePayrollService 
@@ -24,14 +22,15 @@ public class EmployeePayrollService implements IEmployeePayrollService
 	@Override
 	public EmployeePayrollData getEmployeePayrollDataById(int employeeId) 
 	{
-		return employeePayrollList.get(employeeId - 1);
+		return employeePayrollList.stream().filter(empData -> empData.getEmployeeId() == employeeId).findFirst()
+				.orElseThrow(() -> new EmployeePayrollException("Employee Not Found"));
 	}
 
 	@Override
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) 
 	{
-		EmployeePayrollData employeePayrollData = new EmployeePayrollData(
-				employeePayrollList.size() + 1,employeePayrollDTO);
+		EmployeePayrollData employeePayrollData = new EmployeePayrollData(employeePayrollList.size() + 1,
+				employeePayrollDTO);
 		employeePayrollList.add(employeePayrollData);
 		return employeePayrollData;
 	}
@@ -39,6 +38,8 @@ public class EmployeePayrollService implements IEmployeePayrollService
 	@Override
 	public EmployeePayrollData updateEmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) 
 	{
+		employeePayrollList.stream().filter(empData -> empData.getEmployeeId() == employeeId).findFirst()
+				.orElseThrow(() -> new EmployeePayrollException("Employee Not Found"));
 		EmployeePayrollData employeePayrollData = this.getEmployeePayrollDataById(employeeId);
 		employeePayrollData.setName(employeePayrollDTO.name);
 		employeePayrollData.setSalary(employeePayrollDTO.salary);
@@ -49,6 +50,8 @@ public class EmployeePayrollService implements IEmployeePayrollService
 	@Override
 	public void deleteEmployeePayrollData(int employeeId) 
 	{
+		employeePayrollList.stream().filter(empData -> empData.getEmployeeId() == employeeId).findFirst()
+				.orElseThrow(() -> new EmployeePayrollException("Employee Not Found"));
 		employeePayrollList.remove(employeeId - 1);
 
 	}
