7b5ad44cc51dea03d817709d1be8cf74dd83c7f2
padmini v <padmini.v@perfios.com>
2021-11-04 00:57:12 +0530

    ADDED: Regex to validate name

--- src/main/java/com/bridgelabz/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/dto/EmployeePayrollDTO.java
@@ -1,10 +1,19 @@
 package com.bridgelabz.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import lombok.Data;
 
 public @Data class EmployeePayrollDTO 
 {
+	@NotNull
+	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$",message="Employee Name is Invalid!")
 	public String name;
+	
+	@NotNull
+	@Min(value=500,message="Minimum wage should be more than 500")
 	public long salary;
 
 	public EmployeePayrollDTO(String name, long salary) 
