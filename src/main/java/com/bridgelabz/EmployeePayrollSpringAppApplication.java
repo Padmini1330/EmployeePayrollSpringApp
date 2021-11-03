04695d17d77beba0717f008c5a479b6d296c0c9e
padmini v <padmini.v@perfios.com>
2021-11-03 22:45:20 +0530

    ADDED: Lombok for logging

--- src/main/java/com/bridgelabz/EmployeePayrollSpringAppApplication.java
+++ src/main/java/com/bridgelabz/EmployeePayrollSpringAppApplication.java
@@ -2,13 +2,26 @@ package com.bridgelabz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.Data;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
+@Data
 public class EmployeePayrollSpringAppApplication 
 {
 	public static void main(String[] args) 
 	{
-		SpringApplication.run(EmployeePayrollSpringAppApplication.class, args);
+		  ApplicationContext context=SpringApplication.run(EmployeePayrollSpringAppApplication.class, args);
+		  
+		  log.info("Employee App Started in {} Environment"
+				  ,context.getEnvironment().getProperty("environment"));
 	}
 
 }
+
+
+
