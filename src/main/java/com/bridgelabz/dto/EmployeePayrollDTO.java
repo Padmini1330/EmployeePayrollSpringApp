2f7443b283457cf42c010d2c3ab189c552734179
padmini v <padmini.v@perfios.com>
2021-11-07 20:53:40 +0530

    ADDED: remaining employee attributes to dto class

--- src/main/java/com/bridgelabz/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/dto/EmployeePayrollDTO.java
@@ -1,12 +1,15 @@
 package com.bridgelabz.dto;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
-import lombok.Data;
+import lombok.ToString;
 
-public @Data class EmployeePayrollDTO 
+public @ToString class EmployeePayrollDTO 
 {
 	@NotNull
 	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$",message="Employee Name is Invalid!")
@@ -16,10 +19,10 @@ public @Data class EmployeePayrollDTO
 	@Min(value=500,message="Minimum wage should be more than 500")
 	public long salary;
 
-	public EmployeePayrollDTO(String name, long salary) 
-	{
-		this.name = name;
-		this.salary = salary;
-	}
-
+	public String gender;
+	public LocalDate startDate;
+	public String note;
+	public String profilePic;
+	public List<String> department;
+	
 }
