4ceb10009600c0a9d3e345714aee5c0a4ebc793d
padmini v <padmini.v@perfios.com>
2021-11-07 20:52:50 +0530

    ADDED: remaining employee attributes

--- src/main/java/com/bridgelabz/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/model/EmployeePayrollData.java
@@ -1,5 +1,8 @@
 package com.bridgelabz.model;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import com.bridgelabz.dto.EmployeePayrollDTO;
 import lombok.Data;
 
@@ -8,6 +11,12 @@ public @Data class EmployeePayrollData
 	private int employeeId;
 	private String name;
 	private long salary;
+	
+	public String gender;
+	public LocalDate startDate;
+	public String note;
+	public String profilePic;
+	public List<String> departments;
 
 	public EmployeePayrollData() 
 	{
@@ -18,5 +27,10 @@ public @Data class EmployeePayrollData
 		this.employeeId = employeeId;
 		this.name = employeePayrollDTO.name;
 		this.salary = employeePayrollDTO.salary;
+		this.gender = employeePayrollDTO.gender;
+		this.startDate = employeePayrollDTO.startDate;
+		this.note = employeePayrollDTO.note;
+		this.profilePic = employeePayrollDTO.profilePic;
+		this.departments = employeePayrollDTO.department;
 	}
 }
