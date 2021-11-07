543e6027ea32a9ed05f8567b4ff32975c1505181
padmini v <padmini.v@perfios.com>
2021-11-07 21:30:32 +0530

    ADDED: Annotations to create table out of EmployeePayroll class

--- src/main/java/com/bridgelabz/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/model/EmployeePayrollData.java
@@ -3,34 +3,59 @@ package com.bridgelabz.model;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
 import com.bridgelabz.dto.EmployeePayrollDTO;
 import lombok.Data;
 
+@Entity
+@Table(name = "employee_payroll")
 public @Data class EmployeePayrollData 
 {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "employee_id")
 	private int employeeId;
+
+	@Column(name = "name")
 	private String name;
 	private long salary;
+	private String gender;
+	@Column(name = "start_date")
+	private LocalDate startDate;
+	private String note;
+	@Column(name = "profile_pic")
+	private String profilePic;
 	
-	public String gender;
-	public LocalDate startDate;
-	public String note;
-	public String profilePic;
-	public List<String> departments;
+	@ElementCollection
+	@CollectionTable(name = "employee_department", joinColumns = @JoinColumn(name = "id"))
+	@Column(name = "department")
+	private List<String> departments;
 
 	public EmployeePayrollData() 
 	{
 	}
 
-	public EmployeePayrollData(int employeeId, EmployeePayrollDTO employeePayrollDTO) 
+	public EmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) 
 	{
-		this.employeeId = employeeId;
-		this.name = employeePayrollDTO.name;
-		this.salary = employeePayrollDTO.salary;
+		this.updateEmployeePayrollData(employeePayrollDTO);
+	}
+	public EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) 
+	{
+		this.name=employeePayrollDTO.name;
+		this.salary=employeePayrollDTO.salary;
 		this.gender = employeePayrollDTO.gender;
 		this.startDate = employeePayrollDTO.startDate;
 		this.note = employeePayrollDTO.note;
 		this.profilePic = employeePayrollDTO.profilePic;
-		this.departments = employeePayrollDTO.department;
+		this.departments = employeePayrollDTO.departments;
+		return this;
 	}
 }
