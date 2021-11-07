9258ef11694319330a43f481b0e4d25981e7f29a
padmini v <padmini.v@perfios.com>
2021-11-07 21:08:57 +0530

    ADDED: Validations for other employee attributes

--- src/main/java/com/bridgelabz/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/dto/EmployeePayrollDTO.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.ToString;
 
@@ -19,10 +22,24 @@ public @ToString class EmployeePayrollDTO
 	@Min(value=500,message="Minimum wage should be more than 500")
 	public long salary;
 
+	@NotNull
+	@Pattern(regexp = "male|female", message = "Gender has to be male or female")
 	public String gender;
+
+	@JsonFormat(pattern = "dd MMM yyyy")
+	
+	@NotNull(message = "Start Date should not be Null")
+	
+	@PastOrPresent(message = "start date should be past or todays date")
 	public LocalDate startDate;
+
+	@NotNull(message = "Note cannot be Null")
 	public String note;
+
+	@NotNull(message = "ProfilePic cannot not be Null")
 	public String profilePic;
-	public List<String> department;
+
+	@NotNull(message = "department should not be Null")
+	public List<String> departments;
 	
 }
